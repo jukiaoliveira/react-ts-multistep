13ef2a228bfa7a2e28c5ef28c447f14e8f5a5476
Julia OLiveira <jurafaoliveira@gmail.com>
2026-03-26 23:22:57 -0300

    feat: create base component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
+// Components
+import UserForm from './components/UserForm';
+import Review from './components/Review';
+import Thanks from './components/Thanks';
+
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+
+// Hooks
+
+// CSS
 import './App.css'
 
 function App() {
 
   return (
     <div className="app">
-      <h1>React TS multistep form</h1>
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto
+        </p>
+      </div>
+      <div className='form-container'>
+        <p>passos</p>
+        <form>
+          <div className='inputs-container'>
+            <UserForm />
+          </div>
+          <div className='actions'>
+            <button>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button>
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
