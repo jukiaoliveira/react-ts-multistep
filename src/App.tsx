387678a0c83ac39666e5f6a16582c17a8c4f6892
Julia OLiveira <jurafaoliveira@gmail.com>
2026-03-30 22:14:33 -0300

    feat: add review component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Thanks from './components/Thanks';
 import Steps from './components/Steps';
 
 import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+import {FiSend} from 'react-icons/fi'
 
 // Hooks
 import { useState } from 'react';
@@ -40,10 +41,10 @@ function App() {
   const formComponents = [
     <UserForm data={data} updateFieldHandler={updateFieldHandler} />,
     <ReviewForm data={data} updateFieldHandler={updateFieldHandler} />,
-    <Thanks />
+    <Thanks data={data} />
   ]
 
-  const { currentStep, currentComponent, changeStep } = useForm(formComponents);
+  const { currentStep, currentComponent, changeStep, isLastStep } = useForm(formComponents);
 
   return (
     <div className="app">
@@ -62,10 +63,17 @@ function App() {
               <GrFormPrevious />
               <span>Voltar</span>
             </button>
-            <button type='submit'>
-              <span>Avançar</span>
-              <GrFormNext />
-            </button>
+            {!isLastStep ? (
+              <button type="submit">
+                <span>Avançar</span>
+                <GrFormNext />
+              </button>
+            ) : (
+              <button type="button">
+                <span>Enviar</span>
+                <FiSend />
+              </button>
+            )}
           </div>
         </form>
       </div>
